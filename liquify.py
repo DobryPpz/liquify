65200bb27d2402197e5027f6e646c3b8886c4820
Aleksander Modzelewski <xaleksx960@gmail.com>
2024-11-23 08:30:52 +0100

    add the milliliter step to make it easy for users to dose particular fluids

--- liquify.py
+++ liquify.py
@@ -16,14 +16,15 @@ class Fluid:
 class TargetMix(Fluid):
     def __init__(self, target_volume: int | float = 0,
                  error_margin: float = 0.1,
-                 target_concentration: int | float = 0):
+                 target_concentration: int | float = 0,
+                 milliliter_step: int | float = 1):
         super().__init__(target_volume)
         self.taste_set = set()
         self.fluid_list = []
         self.total_volume = 0
         self.error_margin = error_margin
         self.target_concentration = target_concentration
-        self.step_list = []
+        self.milliliter_step = milliliter_step
 
     def add_fluid(self, fluid: Fluid):
         if type(fluid) is not Fluid:
@@ -32,3 +33,14 @@ class TargetMix(Fluid):
         self.fluid_list.append(fluid)
         self.total_volume += fluid.volume
         return self
+
+    def is_possible(self, taste_mix: tuple,
+                    current_volume: float,
+                    current_conc: float,
+                    which_fluid_list: list,
+                    memo: dict,
+                    index: int):
+        pass
+
+    def calculate(self):
+        which_fluid_list = []
