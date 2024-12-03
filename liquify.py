3f1044249b3c84ed029ebc33263c929a6c06ca21
Aleksander Modzelewski <xaleksx960@gmail.com>
2024-12-04 00:28:09 +0100

    add license

--- liquify.py
+++ liquify.py
@@ -1,3 +1,22 @@
+"""
+    Liquify: An e-liquid mixer
+    Copyright (C) 2024  Aleksander Modzelewski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 class Fluid:
     def __init__(self, volume: int | float = 0,
                  nicotine_concentration: int | float = 0,
